2bb2a9fa45f58ddd29d6219e8f36b11542c1e66e
Alexey Gulyaev <algulaev@mail.ru>
2020-09-16 17:55:09 +0300

    циклы

--- js/script.js
+++ js/script.js
@@ -20,20 +20,40 @@
 
 // console.log(personalMovieDB);
 
-const num = 50;
-(num == 50)? console.log("Ok"): console.log("Error");
+// const num = 50;
+// (num == 50)? console.log("Ok"): console.log("Error");
 
-switch (num) {
-    case 49:
-        console.log("Неверно");
-        break;
-    case 100:
-        console.log("Неверно");
-        break;
-    case 50:
-        console.log("Ok");
-        break;
-    default:
-        console.log("Не в этот раз");
-        break;     
+// switch (num) {
+//     case 49:
+//         console.log("Неверно");
+//         break;
+//     case 100:
+//         console.log("Неверно");
+//         break;
+//     case 50:
+//         console.log("Ok");
+//         break;
+//     default:
+//         console.log("Не в этот раз");
+//         break;     
+// };
+
+let num = 50;
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// };
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue; // пропустит шаг, т.е. значение 6 не выведет в консоль
+    }
+    console.log(i);
 }
