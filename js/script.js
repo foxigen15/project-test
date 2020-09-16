63fcc2405bf542e0ee1f346da1b754a1df4a6e57
Alexey Gulyaev <algulaev@mail.ru>
2020-09-16 18:55:32 +0300

    HW2 задание с просмотренными фильмами, проверка на null, длину строки, запись в объект

--- js/script.js
+++ js/script.js
@@ -1,59 +1,32 @@
 "use sctrict";
 
-// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const a = prompt("Один из последних просмотренных фильмов", ""),
-//       b = +prompt("На сколько баллов вы его оцениваете?", ""),
-//       c = prompt("Один из последних просмотренных фильмов", ""),
-//       d = +prompt("На сколько баллов вы его оцениваете?", "");
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-// const num = 50;
-// (num == 50)? console.log("Ok"): console.log("Error");
-
-// switch (num) {
-//     case 49:
-//         console.log("Неверно");
-//         break;
-//     case 100:
-//         console.log("Неверно");
-//         break;
-//     case 50:
-//         console.log("Ok");
-//         break;
-//     default:
-//         console.log("Не в этот раз");
-//         break;     
-// };
-
-let num = 50;
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// };
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue; // пропустит шаг, т.е. значение 6 не выведет в консоль
-    }
-    console.log(i);
+let i = 1;
+while (i <= numberOfFilms){
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+          b = prompt("На сколько баллов вы его оцениваете?", "");
+    if (a != "" && a != null && a.length <= 50 && b != "" && b != null){
+        personalMovieDB.movies[a] = b;
+        i++;
+    } else {console.log("error");}
 }
+
+if (personalMovieDB.count < 10){
+    console.log('Слишком мало посмотрел');
+} else if (personalMovieDB.count >10 && personalMovieDB.count <=50){
+    console.log('Вы классический зритель');
+} else{
+    console.log('Вы киноман');
+}
+
+console.log(personalMovieDB);
+
