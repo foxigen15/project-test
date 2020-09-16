40c943c1d8378e5d62881795c55f3cd8fe67afbf
Alexey Gulyaev <algulaev@mail.ru>
2020-09-16 17:34:58 +0300

    условные операторы тирнарный и switch

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 "use sctrict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-      b = +prompt("На сколько баллов вы его оцениваете?", ""),
-      c = prompt("Один из последних просмотренных фильмов", ""),
-      d = +prompt("На сколько баллов вы его оцениваете?", "");
+// const a = prompt("Один из последних просмотренных фильмов", ""),
+//       b = +prompt("На сколько баллов вы его оцениваете?", ""),
+//       c = prompt("Один из последних просмотренных фильмов", ""),
+//       d = +prompt("На сколько баллов вы его оцениваете?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+const num = 50;
+(num == 50)? console.log("Ok"): console.log("Error");
+
+switch (num) {
+    case 49:
+        console.log("Неверно");
+        break;
+    case 100:
+        console.log("Неверно");
+        break;
+    case 50:
+        console.log("Ok");
+        break;
+    default:
+        console.log("Не в этот раз");
+        break;     
+}
